a94175154899f19ed033dd3c6bdb14271cbfc1bb
andrea <andrea.bellucci10@libero.it>
2024-06-03 16:19:48 +0200

    spring initializer

---
+++ src/main/java/com/example/demo/ProfessionalCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalCoreServiceApplication.class, args);
+	}
+
+}
