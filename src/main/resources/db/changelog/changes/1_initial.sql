6a87f855fb5b8d1d3956a10d8ffcb2fa05d78069
Rufi, Edoardo (contracted) <Edoardo.Rufi@contracted.pmi.com>
2024-06-04 10:14:26 +0200

    rename

--- src/main/resources/db/changelog/changes/1_initial.sql
+++ src/main/resources/db/changelog/changes/1_initial.sql
@@ -1,3 +1,2 @@
 DROP SCHEMA IF EXISTS professional_dev CASCADE;
-CREATE SCHEMA IF NOT EXISTS professional_dev;
-ALTER SCHEMA professional_dev OWNER TO postgres; --pport 5432
+CREATE SCHEMA IF NOT EXISTS professional_dev AUTHORIZATION postgres;
